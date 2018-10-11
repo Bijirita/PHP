4d2eea8796d726ba9e6b49320ec9688d568350b0
Bijirita <bijirita009@gmail.com>
2018-10-11 17:55:06 -0400

    discover Japan

--- index.php
+++ index.php
@@ -1,80 +1,43 @@
-<?php
-    //Starting a session
-    session_start();
-?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+        <title>Discover Japan</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
     </head>
     <body>
-        <?php
-            echo ("Here I create a file called example.txt, write 'try this fgetc, close it, reopen it <br> echo each character, then close");
-            $file = fopen("example.txt","w");
-            fwrite ($file, "try this fgetc");
-            fclose ($file);
-            $fileGet = fopen("example.txt", "r");
-            echo "<br>";
-            while (! feof ($fileGet)) {
-                echo fgetc($fileGet);
-            }
+        <section class="jumbotron text-center"> 
+            <h1>Japan<h1>
+        </section>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <a class="navbar-brand" <img src ="../Assets/Images/"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Explore</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Flights</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>    
+                </ul>
+            </div>  
+        </nav>
 
-            fclose($fileGet);
-        ?>
-    <form method="post" action ="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <h1>Please fill out this form</h1>
-        <label> 
-            First Name
-            <input type="text" name="First-Name" required>
-            <?php
-                echo "<br>"; echo "<br>";
-                $cookie_FirstName = "First_Name";
-                $cookie_FirstNameValue = ($_POST["First-Name"]);
-                echo "<br>"; echo "<br>";
-                setcookie($cookie_FirstName, $cookie_FirstNameValue, time () + (86400 * 30), "/");
-            ?>
-            <?php
-            // Test for POST submission
-            // Test for COOKIE set
-                if (!isset($cookie_FirstNameValue)) {
-                    echo "Your cookie name " . $cookie_FirstName . " is not set.";
-                } else {
-                    echo "Your cookie is set " . $cookie_FirstName . "   " .$cookie_FirstNameValue;
-                }
-            ?>
-        </label>
-    
-
-        <?php
-
-        //---------------------------session--------------------------------//
-            echo "<br>" ; echo "<br>";echo "<br>" ; echo "<br>";
-            $_SESSION["favcolor"] = "green";
-            $_SESSION["favanimal"] = "cat";
-            echo "<b>Session variables are set.</b>";
-            echo "<br>" ; echo "<br>";
-            echo "<b>Going to display session variables</b>";
-            echo "<br>";
-            print_r ($_SESSION);
-            echo "<br>" ; echo "<br>";
-            echo "<b>overwriting favcolor</b>";
-            echo "<br>";
-            $_SESSION["favcolor"] = "blue";
-            print_r ($_SESSION);
-            echo "<br>";echo "<br>";
-            echo "<b>removing session variables</b>";
-            echo "<br>";
-            session_unset();
-            print_r ($_SESSION);
-            echo "<br>"; echo "<br>";
-            echo "an individual session value cannot be removed with session unset, as it <b>only</b> removes all variables. <br> So, the following <b>will</b> break.";
-            session_unset($_SESSION["favcolor"]);
-            echo "<br>"; echo "<br>";
-            echo "<b>COOL!</b>";
-        ?>
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
     </body>
 </html>
