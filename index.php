072588e41c4596bb43f55ff53fdbc67b4af8a01f
Bijirita <bijirita009@gmail.com>
2018-10-11 09:27:54 -0400

    setting up heroku

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="Assets\Stylesheets\main.css">
+    <link rel="stylesheet" href="Assets\Stylesheets\reset.css">
+    <title>PHPProject</title>
+</head>
+<body>
+    
+</body>
+</html>
